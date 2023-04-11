0f53cb48de529f9e992cd891bd79ebbca84c66c2
Abhishek Vijayakumar <Abhishekwhizzer@gmail.com>
2023-04-10 23:13:42 -0400

    added form validations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   genders = ['male', 'female'];
+  signupForm: FormGroup;
+  forbiddenUserName = ['anna', 'Maria'];
+
+  ngOnInit(): void {
+    this.signupForm = new FormGroup({
+      userName: new FormControl(null, [
+        Validators.required,
+        this.forbiddenNames.bind(this),
+      ]),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      gender: new FormControl('male', Validators.required),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.signupForm);
+  }
+
+  forbiddenNames(control: FormControl): { [s: string]: boolean } {
+    if (this.forbiddenUserName.indexOf(control.value) !== -1) {
+      return { namesForbidden: true };
+    }
+    return null;
+  }
 }
